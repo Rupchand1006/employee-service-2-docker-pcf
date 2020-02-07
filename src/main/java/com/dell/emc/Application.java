82d8724e51370407e9c242713705f6c1ad00f9af
Dewangan <Rupchand_Dewangan@Dell.com>
2020-02-07 10:52:11 +0530

    Updated file

--- src/main/java/com/dell/emc/Application.java
+++ src/main/java/com/dell/emc/Application.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
